f5418808d3b2bfa26ebe6f8ddbbdea23dcdb6fd8
dmmills-stu <david.mills2@student.uagc.edu>
2025-07-28 17:28:13 -0500

    Added some icons to the NavBar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -7,10 +7,29 @@ const NavBar = () => {
     <>
       <nav className="navbar">
         <ul className="nav-list">
-          <li><Link href="/">StreamList</Link></li>
-          <li><Link href="/movies">Movies</Link></li>
-          <li><Link href="/cart">Cart</Link></li>
-          <li><Link href="/about">About</Link></li>
+          <li>
+            <Link href="/" className="flex items-center gap-2">
+              <img src="/icons/HomeIcon.svg" alt="Home" className="w-5 h-5" />
+              <span>StreamList</span>
+            </Link>
+          </li>
+          <li>
+            <Link href="/movies" className="flex items-center gap-2">
+              <img src="/icons/MovieIcon.svg" alt="Home" className="w-5 h-5" />
+              <span>Movies</span>
+            </Link></li>
+          <li>
+            <Link href="/cart" className="flex items-center gap-2">
+              <img src="/icons/CartIcon.svg" alt="Home" className="w-5 h-5" />
+              <span>Cart</span>
+            </Link>
+          </li>
+          <li>
+            <Link href="/about" className="flex items-center gap-2">
+              <img src="/icons/AboutIcon.svg" alt="Home" className="w-5 h-5" />
+              <span>About</span>
+            </Link>
+          </li>
         </ul>
       </nav>
       <hr />

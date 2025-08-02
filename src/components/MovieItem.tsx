4cb3b9437af867d36c0604ca9d1e1a196526813b
dmmills-stu <david.mills2@student.uagc.edu>
2025-08-02 17:07:24 -0500

    Changed Google Fonts icons using SVGs to react-icons.

--- src/components/MovieItem.tsx
+++ src/components/MovieItem.tsx
@@ -1,14 +1,12 @@
 import { useState } from "react";
 import { MovieItemProps } from "../types/MovieItemProps";
+import { IconContext } from "react-icons";
+import { MdEdit, MdCancel, MdDelete, MdCheckBoxOutlineBlank, MdCheckBox } from "react-icons/md";
+import { AiFillSave } from "react-icons/ai";
 
 const MovieItem: React.FC<MovieItemProps> = ({ title, watched, inEdit, onWatched, onDelete, onItemSave }) => {
   const [isEditing, setIsEditing] = useState(inEdit);
   const [editedTitle, setEditedTitle] = useState(title);
-
-  // Because the checkbox is not conditionally rendered, I decided to change the SVG path based on whether or not the box was checked.
-  const checkboxPath = watched
-    ? "m424-312 282-282-56-56-226 226-114-114-56 56 170 170ZM200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h560q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H200Zm0-80h560v-560H200v560Zm0-560v560-560Z"
-    : "M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h560q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H200Zm0-80h560v-560H200v560Z";
 
   // Without this, the input when editing will not update, making it effectively read-only.
   const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -35,9 +33,13 @@ const MovieItem: React.FC<MovieItemProps> = ({ title, watched, inEdit, onWatched
   return (
     <li className="movie-item">
       <button onClick={onWatched} className="col-watched">
-        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#ffffff">
-            <path d={checkboxPath} />
-        </svg>
+        <IconContext.Provider value={{ className: "react-icon" }}>
+          { watched ? (
+            <MdCheckBox />
+          ) : (
+            <MdCheckBoxOutlineBlank />
+          ) }
+        </IconContext.Provider>
       </button>
       {isEditing ? (
         <input 
@@ -57,15 +59,15 @@ const MovieItem: React.FC<MovieItemProps> = ({ title, watched, inEdit, onWatched
           <span>
             Save
           </span>
-          <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#ffffff">
-            <path d="M840-680v480q0 33-23.5 56.5T760-120H200q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h480l160 160Zm-80 34L646-760H200v560h560v-446ZM480-240q50 0 85-35t35-85q0-50-35-85t-85-35q-50 0-85 35t-35 85q0 50 35 85t85 35ZM240-560h360v-160H240v160Zm-40-86v446-560 114Z"/>
-          </svg>
+          <IconContext.Provider value={{ className: "react-icon" }}>
+            <AiFillSave />
+          </IconContext.Provider>
         </button>
       ) : (
         <button onClick={handleEdit} className="col-edit">
-          <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#ffffff">
-            <path d="M200-200h57l391-391-57-57-391 391v57Zm-80 80v-170l528-527q12-11 26.5-17t30.5-6q16 0 31 6t26 18l55 56q12 11 17.5 26t5.5 30q0 16-5.5 30.5T817-647L290-120H120Zm640-584-56-56 56 56Zm-141 85-28-29 57 57-29-28Z"/>
-          </svg>
+          <IconContext.Provider value={{ className: "react-icon" }}>
+            <MdEdit />
+          </IconContext.Provider>
         </button>
       )}
       {isEditing ? (
@@ -73,15 +75,15 @@ const MovieItem: React.FC<MovieItemProps> = ({ title, watched, inEdit, onWatched
           <span>
             Cancel
           </span>
-          <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#ffffff">
-            <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/>
-          </svg>
+          <IconContext.Provider value={{ className: "react-icon" }}>
+            <MdCancel />
+          </IconContext.Provider>
         </button>
       ) : (
         <button onClick={onDelete} className="col-delete">
-          <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#ffffff">
-            <path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/>
-          </svg>
+          <IconContext.Provider value={{ className: "react-icon" }}>
+            <MdDelete />
+          </IconContext.Provider>
         </button>
       )}
     </li>
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,6 +1,12 @@
-import React, { ReactNode } from 'react';
+'use client'; // Needed for IconContext.
+
+import React from 'react';
 import Link from 'next/link';
 import '../styles/NavBar.css';
+import { IconContext } from 'react-icons';
+import { FaHome, FaShoppingCart } from 'react-icons/fa';
+import { MdMovie } from 'react-icons/md';
+import { IoMdInformationCircle } from 'react-icons/io';
 
 const NavBar = () => {
   return (
@@ -10,36 +16,36 @@ const NavBar = () => {
           <div className="outside-spacer"></div>
           <li>
             <Link href="/">
-              <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#ffffff">
-                <path d="M240-200h120v-240h240v240h120v-360L480-740 240-560v360Zm-80 80v-480l320-240 320 240v480H520v-240h-80v240H160Zm320-350Z"/>
-              </svg>
+              <IconContext.Provider value={{ className: "react-icon" }}>
+                <FaHome />
+              </IconContext.Provider>
               <span>StreamList</span>
             </Link>
           </li>
           <div className="inside-spacer"></div>
           <li>
             <Link href="/movies">
-              <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#ffffff">
-                <path d="m160-800 80 160h120l-80-160h80l80 160h120l-80-160h80l80 160h120l-80-160h120q33 0 56.5 23.5T880-720v480q0 33-23.5 56.5T800-160H160q-33 0-56.5-23.5T80-240v-480q0-33 23.5-56.5T160-800Zm0 240v320h640v-320H160Zm0 0v320-320Z"/>
-              </svg>
+              <IconContext.Provider value={{ className: "react-icon" }}>
+                <MdMovie />
+              </IconContext.Provider>
               <span>Movies</span>
             </Link>
           </li>
           <div className="inside-spacer"></div>
           <li>
             <Link href="/cart">
-              <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#ffffff">
-                <path d="M280-80q-33 0-56.5-23.5T200-160q0-33 23.5-56.5T280-240q33 0 56.5 23.5T360-160q0 33-23.5 56.5T280-80Zm400 0q-33 0-56.5-23.5T600-160q0-33 23.5-56.5T680-240q33 0 56.5 23.5T760-160q0 33-23.5 56.5T680-80ZM246-720l96 200h280l110-200H246Zm-38-80h590q23 0 35 20.5t1 41.5L692-482q-11 20-29.5 31T622-440H324l-44 80h480v80H280q-45 0-68-39.5t-2-78.5l54-98-144-304H40v-80h130l38 80Zm134 280h280-280Z"/>
-              </svg>
+              <IconContext.Provider value={{ className: "react-icon" }}>
+                <FaShoppingCart />
+              </IconContext.Provider>
               <span>Cart</span>
             </Link>
           </li>
           <div className="inside-spacer"></div>
           <li>
             <Link href="/about">
-              <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#ffffff">
-                <path d="M440-280h80v-240h-80v240Zm40-320q17 0 28.5-11.5T520-640q0-17-11.5-28.5T480-680q-17 0-28.5 11.5T440-640q0 17 11.5 28.5T480-600Zm0 520q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/>
-              </svg>
+              <IconContext.Provider value={{ className: "react-icon" }}>
+                <IoMdInformationCircle />
+              </IconContext.Provider>
               <span>About</span>
             </Link>
           </li>
